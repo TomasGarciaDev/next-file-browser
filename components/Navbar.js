dcbdca4690cd47acf94b929236d36306d9c91096
Tomas Garcia <tomasgarciadev@gmail.com>
2022-07-11 17:41:49 +0200

    Body Component created

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,11 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
+  const searchIcon = <AiOutlineSearch />
   return (
     <nav className={styles.container}>
       <h1>File Browser</h1>
-      <input type='text' className={styles.inputBox}/>
+      <input type='text' className={styles.inputBox} placeholder="Search"/>
     </nav>
   )
 }
