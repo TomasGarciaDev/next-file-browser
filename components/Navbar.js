15a336e9781c610190115d1441061aa955af8ec9
Tomas Garcia <tomasgarciadev@gmail.com>
2022-07-11 15:29:33 +0200

    Navbar.module.css created for Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import { AiOutlineSearch } from 'react-icons/ai';
+import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className={styles.container}>
       <h1>File Browser</h1>
-      <input type='text' />
+      <input type='text' className={styles.inputBox}/>
     </nav>
   )
 }
